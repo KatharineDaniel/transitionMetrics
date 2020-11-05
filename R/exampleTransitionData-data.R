2bb2afff8fe08075c47831af5bd4d00874846bbc
KatharineDaniel <ked4f@virginia.edu>
2020-11-05 17:53:53 -0500

    documentation for example data upload

---
+++ R/exampleTransitionData-data.R
@@ -0,0 +1,16 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(exampleTransitionData)
+#'
+#' @format A data frame: Example data structure needed for transition array analysis.
+#'
+#' @examples
+#' data(exampleTransitionData)
+"exampleTransitionData"
